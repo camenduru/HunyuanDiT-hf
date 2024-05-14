8c00938d817195d372eac0fdaf65547b44094492
Apolinário from multimodal AI art <multimodalart@users.noreply.huggingface.co>
2024-05-14 08:52:54 +0000

    Update app/hydit_app.py

--- app/hydit_app.py
+++ app/hydit_app.py
@@ -1,7 +1,7 @@
 import os
 from huggingface_hub import snapshot_download
 os.makedirs("/home/user/app/ckpts", exist_ok=True)
-snapshot_download(repo_id="Tencent-Hunyuan/HunyuanDiT", local_folder="/home/user/app/ckpts")
+snapshot_download(repo_id="Tencent-Hunyuan/HunyuanDiT", local_dir="/home/user/app/ckpts")
 
 import gradio as gr
 import pandas as pd
